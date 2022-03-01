532c7d540546621a0ac4a569abd410ae4695f68d
Jawad-11 <62426645+Jawad-11@users.noreply.github.com>
2022-02-28 18:11:49 -0800

    Add files via upload
    here is some codes related to basic of python

---
+++ prectice.py
@@ -0,0 +1,79 @@
+
+
+"""
+
+a = input("Enter the number here")
+
+b = input("enter a new number")
+
+
+print( "sum is"  , (int(a) + int(b) )/2)
+"""
+
+
+
+
+# prectice
+"""
+
+a = 6
+
+b=7
+
+c = a>b
+print(c)
+"""
+
+""".......inout from user............
+
+a =32
+
+b = "harrythepagal"
+
+print(type(a))
+
+print(b.__len__())
+
+print(b[3])
+
+""" """ we can only access but could not edit """
+
+# print(b[0:3])  > this is string slycing
+
+
+""" .......if we have the in this order than this is list 
+
+  x = ["apple" , "banana"]
+
+  tuple >>>
+
+  x =("fjilf " , " dkfsdh ")
+  """
+
+#  d = b[0::2]>> use for skip of 1
+
+# print(d)
+
+
+#"""write string from index 2 to index 5 """
+#txt = '''hellw world' '''
+
+
+#x = txt[2:5]>-- this is use writing string from index 2 to index 5 
+
+#print(x)
+
+#y=txt.strip()>-- for removing spaces at font and end of string 
+#print(y)
+#txt= "jdch jkdjz "
+#txt = txt.upper()
+#print(txt)
+
+#....................................................#
+story = "i am starting programming "
+print(len(story))
+
+
+
+
+
