1fd9fc83147df1ff352b08e5128ed10c064d4217
Jawad-11 <62426645+Jawad-11@users.noreply.github.com>
2022-03-01 03:30:28 -0800

    Add files via upload

---
+++ string.py
@@ -0,0 +1,41 @@
+#some functions which we can implement on strings
+"""story = "i will do work"
+print(len(story))
+print(story.endswith("fsfd"))
+print(story.count("w")) #.>...for counting char or any string
+print(story.capitalize())
+print(story.find("jawad")) #>.. this is exist or not
+print(story.find("will"))
+print(story.replace("do", "done"))
+
+#escape sequece /n,/t
+escap = "i am jawad . \n and we \t we will work"
+print(escap)"""
+                              #prctice set
+#!>>
+#x = input("enter the name: \n ")
+
+#print("GOOD AGTERNOON",x)
+    #2>>
+"""letter = ''' Dear <[name]> ,
+ congratulations for coding 
+  <date> ''' 
+name = input("enter the name:")
+date = input("enter the date::")
+letter = letter.replace("<[name]>", name)
+letter = letter.replace("<date>", date)
+print(letter) """
+
+#  3>> write program to detect doublespaces
+str = " i am  walla"
+doublespaces = str.find("  ")
+print(doublespaces)
+#4>>escape sequence \n .,\t
+
+
+
+
+
+
+
+
