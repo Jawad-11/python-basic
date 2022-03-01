30a781a00bc9e1bc3ad10f291dc2c512f6e43529
Jawad-11 <62426645+Jawad-11@users.noreply.github.com>
2022-03-01 04:09:07 -0800

    Add files via upload
    lists and its method

---
+++ list.py
@@ -0,0 +1,20 @@
+#List >> this will always in order
+a = [2 , 34 , 33 , 36 , 99]
+a[2] = 55
+print(a)
+#we can also create of list of different types
+b= ["jawad" , 2 , 2.99 , False]
+print(b)
+#list slicing
+print(b[0:2])
+print(b[-2:])
+
+#         .........LIst Methods  ........
+l1 = [1,8,7,21,32]
+#l1.sort()>> for ascending order sort
+#l1.reverse()  >> from end to first
+#l1.append(45)  >>adds values in l1(important)
+#l1.insert(2, 61) >> insert 61 on index 2
+#l1.pop(3)        >>remove the valueof iindex 3
+#l1.remove(8) >>remove only that which mention
+#print((l1))
