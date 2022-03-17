407e0647501510d68b0d0491c4882a708d1c7594
Jawad-11 <62426645+Jawad-11@users.noreply.github.com>
2022-03-17 13:05:44 -0700

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,62 @@
+# a  = int(input("enter the value::"))
+# if(a==0):
+#      print("it is zero")
+# elif(a<10):
+#      print("a is between 3 and 5")
+# else:
+#     print("is out of range")
+
+   #write a program when age is greater or equal to 18
+# b = int(input("enter the age ::>"))
+# if(b==18):
+    #     print("age is alloweed")
+# if(b>=18): #good wAY FOR CONDITION
+#     print("age is alloweed")
+     # elif(b>18):
+#     print("age is allowed")
+# else :
+#     print("underage student")
+#FOR using condition of and we use and else than sign in python
+  #### is and in in python
+# c = None
+# if(c is None)
+# print("yes")
+# else :
+#   print("no")
+    #in 
+# a = {22 , 66,33,66}
+# if(22 in a):
+#   print("yses")
+# else:
+#   print("no")
+                       #practice set
+                #print the greater one
+# g = int(input("enter the first number::"))
+# h = int(input("enter the second number::"))
+# i = int(input("enter the third number::"))
+# j = int(input("enter the forth number::"))
+# if(g>h and g>i and g>j):
+#   print("value of g is greater")
+# elif(h>g and h>i and h>j):
+#   print("value of h is greater")
+# elif(i>h and i>j and i>g):
+#   print("value of i is greater")
+
+# elif(j>h and j>i and j>g):
+#   print("value of j is greater")
+# else :
+#     print("no condition fullfill")
+         #2>> input is spam or not
+# text = input("enter the text")
+# if("atta" in text):
+#   notallowed = True
+# elif("jawad" in text):
+#   notallowed = True
+# elif("shoaib" in text):
+#     notallowed = True
+# else :
+#       notallowed = False
+# if(notallowed):
+#    print("this text is spam")
+# else :
+#      print("this text is not spam")
