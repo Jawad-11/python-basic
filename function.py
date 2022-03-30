d4abf71da49eb98903121b9f028f2863a869174a
Jawad-11 <62426645+Jawad-11@users.noreply.github.com>
2022-03-30 04:55:59 -0700

    Add files via upload
    with practice set

---
+++ function.py
@@ -0,0 +1,41 @@
+# def greet(name) :
+#     print("good night! ", name)
+
+# greet("jawad")
+
+# def mysum(num1 ,num2):
+#    y = num1 +num2
+#    return y
+# s =mysum(2, 555)
+# print(s)
+
+#default parameter value
+# def slaam(name = "jawad"):
+#     return ("good day" , name)
+
+# u =slaam()
+# print(u)
+               #practice set
+      #1::> write program with function to get greater number in set
+# def greet(num1 , num2 ,num3):
+#     if(num1>num2 and num1>num3):
+#         print("no 1 is greater")
+#     elif(num2>num1 and num2>num3):
+#         print("no 2 is greater ")
+#     elif(num3>num1 and num3>num2):
+#         print("no 3 is greater")
+
+# greet(100, 8, 9)
+      #2 write a program to convert celcius to farenheight
+# celc = int(input("enter the value::>"))
+# def convrtr(celc):
+#     y=((celc * 9/5) + 32)
+#     return y
+# x =convrtr(celc)
+# print(x)
+      #write program to print after first print
+print("hello", end="")
+print("are",end="")
+print("you",end="")
+print("how",end="")
+
