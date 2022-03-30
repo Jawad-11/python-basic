9df66fc9c23f48e13f257acc9b889768640b947f
Jawad-11 <62426645+Jawad-11@users.noreply.github.com>
2022-03-30 04:55:25 -0700

    Add files via upload
    here i try to discuss all concepts of loops in python

---
+++ loop.py
@@ -0,0 +1,79 @@
+# #while loop firstly
+# i = int(input("enter the value:>"))
+
+# while i <10 :
+#     print("yes"+str(i)) 
+#     i = i+1
+                #print 1 to 50 by the help of while loop
+# j = 1
+# while j <= 50 :
+#     print(j)
+#     j = j+1
+   #write a program to print list 
+# a = ['jawad', '44' , 'saad', 'False']
+# # i=0
+# # while i<len(a) :
+# #     print(a[i])
+# #     i = i+1
+# for b in a :
+#     print(b)
+         #range funtion in python
+# for z in range(3,8):
+#    print(z)
+
+                                      #practice set for loop
+         #1>> write a program which print a multiplication table
+# e = int(input("enter the number"))
+# for g in range(11) :
+#    print(e,"*",g ,"==",(e * g))
+#    g= g+1
+          #2>>write a program that great all names start with s
+# l = ["jawad" , "Saad" , "Kamran" , "suqlain"]
+# for name in l :
+#   # if name.islower():
+#    if name.startswith('s'):
+#      print(name)
+#      continue
+#    elif name.endswith('n'):
+#     print(name)
+          #3>>implement 1 by using while loop
+# r = int(input("enter the number:>>"))
+# i = 0
+# while i <=10 :
+#    print(str(r),"*",str(i),"==",(r*i))
+#    i = i+1
+          #4>>write a program that idntify whather a integr is prime or not
+# j = int(input("enter the value::>>"))
+# prime = True
+# for i in range(2,j):
+#    if(j%i==0):
+#      prime = False
+#    #   print("this number is prime")
+#      break
+# if prime:
+#    print("this number is prime")
+# else :
+#    print("this is not prime number")
+  #write fectorial of given input
+# k = int(input("enter the value::"))
+# fec  = 1
+# for u in range(1,k+1):
+#    fec = fec * u
+# print(f"the value is =={fec}")
+    #write a progrm to print stars
+# n = 4
+# for l in range(4):
+#    print(" "*(n-l-1) , end=" ")
+#    print("*" *  (2*l+1) , end= " ")
+#    print(" "*(n  -l-1))
+  #write a program to print in reverse table
+o = int(input("enter intger::"))
+for i in range (1,11) : 
+  
+   print(str(o) , "*" , str(i) ,"==",(o*i))
+
+
+
+
+
+
